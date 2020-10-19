da5e4725b93349a71d9d3010b418468b5c8765b5
tracer-nju <tracer@nju.edu.cn>
2020-10-19 23:40:03 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 0354167babd34af7b2a1aed1122c54f0   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  23:40:03 up  5:08,  1 user,  load average: 0.05, 0.38, 0.54

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,7 +1,29 @@
 #include <stdint.h>
 
+uint64_t data[65]={};
+int x=0;
+static inline uint64_t my_mod(uint64_t a,uint64_t m){
+  while(a>=m){
+    int i=0;
+    for(i=0;i<=x;++i){
+      if(data[i]<a) continue;
+      a-=data[i-1];
+      break;
+    }
+  }
+}
+
+static inline void init(uint64_t m){
+  int i;
+  data[0]=m;
+  for(i=1;;i<<=1){
+    if((m<<i) < (m<<(i-1))) break;
+    data[++x]=m<<i;
+  }
+}
+
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
-  static uint64_t l[64]={};
+  uint64_t l[65]={};
   int p=0,i,j;
   while(a>1){
     if(a&1) l[p++]=1;
@@ -14,6 +36,7 @@ uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
     if(l[i]==0) continue;
     ans=ans+(b<<i);
   }
+  init(m);
   ans = ans & (m-1);
   return ans;
 }
