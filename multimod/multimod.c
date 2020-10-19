4b7d28d08c7f472b020e50969cb4e83b35975c6a
tracer-nju <tracer@nju.edu.cn>
2020-10-19 23:06:18 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 0354167babd34af7b2a1aed1122c54f0   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  23:06:19 up  4:35,  1 user,  load average: 0.66, 0.59, 0.51

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,9 +1,20 @@
 #include <stdint.h>
 
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
-  uint64_t ans = 0,i;
-  for(i=0;i<a;++i){
-    ans+=b;
+  static uint64_t l[64]={};
+  int p=0,i,j;
+  while(a>1){
+    if(a&1) l[p++]=1;
+    else l[p++]=0;
+    a>>=1;
+  }
+  l[p]=1;
+  uint64_t ans = 0;
+  for(i=0;i<=p;++i){
+    if(l[i]==0) continue;
+    for(j=0;j<b;++j){
+      ans=ans+(1<<i);
+    }
   }
   ans = ans & (m-1);
   return ans;
