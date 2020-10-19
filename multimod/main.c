fdbe8ba67a72642af81fe7ca7adb1795e8df2e68
tracer-nju <tracer@nju.edu.cn>
2020-10-19 21:35:37 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 0354167babd34af7b2a1aed1122c54f0   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  21:35:37 up  3:04,  1 user,  load average: 0.23, 0.55, 0.82

--- multimod/main.c
+++ multimod/main.c
@@ -2,33 +2,8 @@
 #include <stdio.h>
 #include <inttypes.h>
 
-static inline uint64_t my_mod(uint64_t a,uint64_t m){
-  while(a>=m) a-=m;
-  return a;
-}
 
-static inline uint64_t addmod(uint64_t a,uint64_t b,uint64_t m){
-  return my_mod(a + b , m);
-}
-
-uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
-  static uint64_t l[64]={};
-  int p=0,i,j;
-  while(a>1){
-    if(my_mod(a,2)==0) l[p++]=0;
-    else l[p++]=1;
-    a>>=1;
-  }
-  l[p]=1;
-  uint64_t ans = 0;
-  for(i=0;i<=p;++i){
-    if(l[i]==0) continue;
-    for(j=0;j<b;++j){
-      ans=addmod(ans,1<<i,m);
-    }
-  }
-  return ans;
-}
+uint64_t multimod(uint64_t a, uint64_t b, uint64_t m);
 
 
 void test(uint64_t a, uint64_t b, uint64_t m) {
