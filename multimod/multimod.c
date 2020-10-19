f94d4342a89b4569b6a22f5d5eaf2e5cc32dc273
tracer-nju <tracer@nju.edu.cn>
2020-10-19 22:24:41 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 0354167babd34af7b2a1aed1122c54f0   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  22:24:41 up  3:53,  1 user,  load average: 0.57, 0.42, 0.34

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,19 +1,12 @@
 #include <stdint.h>
 
-static inline uint64_t my_mod(uint64_t a,uint64_t m){
-  while(a>=m) a-=m;
-  return a;
-}
 
-static inline uint64_t addmod(uint64_t a,uint64_t b,uint64_t m){
-  return my_mod(a + b , m);
-}
 
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
   static uint64_t l[64]={};
   int p=0,i,j;
   while(a>1){
-    if(my_mod(a,2)==0) l[p++]=0;
+    if(a<<63) l[p++]=0;
     else l[p++]=1;
     a>>=1;
   }
@@ -22,8 +15,9 @@ uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
   for(i=0;i<=p;++i){
     if(l[i]==0) continue;
     for(j=0;j<b;++j){
-      ans=addmod(ans,1<<i,m);
+      ans=ans+(1<<i);
     }
   }
+  while(ans>=m) ans-=m;
   return ans;
 }
