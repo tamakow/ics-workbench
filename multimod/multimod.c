49bab8ae0b2f7bc6df363316fb39a8d615728598
tracer-nju <tracer@nju.edu.cn>
2020-10-20 21:59:10 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 18a9d50f97cb4437a449652f0c1e473b   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  21:59:10 up 11:55,  1 user,  load average: 1.09, 0.86, 0.57

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -3,33 +3,60 @@
 uint64_t data[70]={0};
 uint64_t temp = 0;
 int x=0;
+
+static inline uint64_t mul(uint64_t a,uint64_t b){
+  uint64_t ans=0;
+  while(a){
+    if(a&1){
+      ans+=b;
+    }
+    b+=b;
+    a>>=1;
+  }
+  return ans;
+}
+
+static inline uint64_t div(uint64_t a,uint64_t b){
+  int k=0,i=0;
+  while(a>=b){
+    if(a>=b<<i){
+      k+=1<<i;
+      a-=b<<i;
+      ++i;
+    }
+    else --i;
+ 
+   }
+   return k;
+}
+
 static inline uint64_t my_mod(uint64_t a,uint64_t m){
-  if(a>=m){
+  /*if(a>=m){
     int i;
     for(i=x;i>=0;--i){
       while(data[i]<=a) a-=data[i];
     }
-  }
-  return a; 
+  }*/
+  return a-mul(div(a,m),m); 
 }
 
-static inline void init(uint64_t m){
+/*static inline void init(uint64_t m){
   int i;
   data[0]=m;
   for(i=1;i<64;++i){
     if((m<<i) < data[i-1]){x=i-1;break;}
     data[i]=(m<<i);    
   }
-}
+}*/
 
 static inline uint64_t addmod(uint64_t a,uint64_t b,uint64_t m){
   if(a+b>=a&&a+b>=b) return my_mod(a+b,m);
-  else return my_mod(my_mod(a+b,m)+temp,m);
+  else return addmod(my_mod(a+b,m),temp,m);
 }
 
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
   uint64_t ans = 0;
-  init(m);
+  //init(m);
   temp = my_mod(my_mod(-1ULL,m)+1,m);
   while(a){
     if(a&1){
