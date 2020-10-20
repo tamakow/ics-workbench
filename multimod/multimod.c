07d56e9571498b390db26cfbb8d1707950356848
tracer-nju <tracer@nju.edu.cn>
2020-10-20 10:19:06 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 18a9d50f97cb4437a449652f0c1e473b   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  10:19:06 up 15 min,  1 user,  load average: 0.27, 0.47, 0.35

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -9,7 +9,6 @@ static inline uint64_t my_mod(uint64_t a,uint64_t m){
       if(data[i]<=a) continue;
       if(i!=0)
       a-=data[i-1];
-      else a-=m;
       break;
     }
   }
@@ -27,21 +26,19 @@ static inline void init(uint64_t m){
 
 static inline uint64_t addmod(uint64_t a,uint64_t b,uint64_t m){
   if(a+b>a&&a+b>b) return my_mod(a+b,m);
-  else return addmod(my_mod(a+b+1,m),my_mod(-1ULL,m),m);
+  else return addmod(my_mod(a+b,m),my_mod(my_mod(-1ULL,m)+1,m),m);
 }
 
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
   uint64_t ans = 0;
-  int p=0,i,j;
+  int i,j;
   init(m);
-  while(a>1){
-    if(a&1)
-      for(i=0;i<b;++i)
-        ans=addmod(ans,1<<p,m);
-    p++;
+  while(a){
+    if(a&1){
+      ans=addmod(ans,b,m);
+      b=addmod(b,b,m);
+    }
     a>>=1;
   }
-  for(i=0;i<b;++i)
-  ans=addmod(ans,1<<p,m);
   return ans;
 }
