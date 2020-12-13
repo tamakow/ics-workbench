06dbf1e2c473d07ffd60f5d5bb56a9391d4690a8
tracer-nju <tracer@nju.edu.cn>
2020-12-13 20:37:15 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 7f95a082ad164fc09f9b0518eefe4a21   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  20:37:15 up  1:20,  1 user,  load average: 0.71, 0.61, 0.73

--- asm/main.c
+++ asm/main.c
@@ -4,7 +4,7 @@
 
 int main() {
   asm_jmp_buf buf;
-  int r = asm_setjmp(buf);
+  int r = 1;//asm_setjmp(buf);
   if (r == 0) {
     assert(asm_add(1234, 5678) == 6912);
     assert(asm_popcnt(0x0123456789abcdefULL) == 32);
