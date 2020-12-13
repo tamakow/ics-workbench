08f27713998b98f3840b5b43573d6afa91814629
tracer-nju <tracer@nju.edu.cn>
2020-12-14 00:12:06 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 7f95a082ad164fc09f9b0518eefe4a21   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  00:12:06 up  4:55,  1 user,  load average: 0.49, 0.81, 0.90

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -17,11 +17,11 @@ int asm_popcnt(uint64_t x) {//无符号64位整数x二进制表示中1的数量
   }
   */
  //先使用 gcc -S 将c代码转换至汇编代码再写内联汇编
-  asm("mov %1, -32(%%rbp);"//mov %rdi -24(%rbp) rdi寄存器中存储x 
-      "movl $0, -4(%%rbp);"
+  asm("mov %1, -0x18(%%rbp);"//mov %rdi -24(%rbp) rdi寄存器中存储x 
+      "movl $0, -0x4(%%rbp);"
       "jmp L2;"
-      "L4: mov -4(%%rbp), %%eax;"//eax 存储 i
-      "mov -32(%%rbp), %%rdx;" //rdx 存储 x
+      "L4: mov -0x4(%%rbp), %%eax;"//eax 存储 i
+      "mov -0x18(%%rbp), %%rdx;" //rdx 存储 x
       "mov %%eax, %%ecx;"//ecx存储i
       "shr %%cl, %%rdx;" //逻辑右移
       "mov %%rdx, %%rax;" 
@@ -29,8 +29,8 @@ int asm_popcnt(uint64_t x) {//无符号64位整数x二进制表示中1的数量
       "test %%rax, %%rax;"
       "je L3;"
       "addl $1, %0;"
-      "L3: addl $1, -4(%%rbp);"
-      "L2: cmpl $63, -4(%%rbp);"
+      "L3: addl $1, -0x4(%%rbp);"
+      "L2: cmpl $63, -0x4(%%rbp);"
       "jle L4;"
       : "+r"(s)
       : "r"(x)
