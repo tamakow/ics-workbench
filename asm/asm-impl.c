cd2577ac4197968ad66c8b59f271384951c5d4dd
tracer-nju <tracer@nju.edu.cn>
2020-12-13 23:55:26 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 7f95a082ad164fc09f9b0518eefe4a21   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  23:55:26 up  4:38,  1 user,  load average: 1.42, 1.09, 0.97

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -17,8 +17,8 @@ int asm_popcnt(uint64_t x) {//无符号64位整数x二进制表示中1的数量
   }
   */
  //先使用 gcc -S 将c代码转换至汇编代码再写内联汇编
-  asm("movq %1, -24(%%rbp);"//mov %rdi -24(%rbp) rdi寄存器中存储x 
-      "movl $0, -8(%%rbp);"
+  asm("mov %1, -24(%%rbp);"//mov %rdi -24(%rbp) rdi寄存器中存储x 
+      "mov $0, -8(%%rbp);"
       "mov $0, -4(%%rbp);" // i
       "jmp L2;"
       "L4: mov -4(%%rbp), %%eax;"//eax 存储 i
@@ -30,7 +30,7 @@ int asm_popcnt(uint64_t x) {//无符号64位整数x二进制表示中1的数量
       "test %%rax, %%rax;"
       "je L3;"
       "addl $1, %0;"
-      "L3: addl $1, -4(%%rbp)"
+      "L3: addl $1, -4(%%rbp);"
       "L2: cmpl $63, -4(%%rbp);"
       "jle L4;"
       : "+r"(s)
