85eecc53ade81e9892a86983901dcddd4ebc2a04
tracer-nju <tracer@nju.edu.cn>
2020-12-13 20:52:31 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 7f95a082ad164fc09f9b0518eefe4a21   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  20:52:32 up  1:35,  1 user,  load average: 0.41, 0.74, 0.81

--- asm/asm.h
+++ asm/asm.h
@@ -2,9 +2,9 @@
 #include <stddef.h>
 
 // TODO: replace with your definition
-//#include <setjmp.h> // remove this
-//#define asm_jmp_buf jmp_buf
-typedef unsigned long int asm_jmp_buf;
+#include <setjmp.h> // remove this
+#define asm_jmp_buf jmp_buf
+//typedef unsigned long int asm_jmp_buf;
 
 int64_t asm_add(int64_t a, int64_t b);
 int     asm_popcnt(uint64_t x);
--- asm/main.c
+++ asm/main.c
@@ -1,11 +1,10 @@
 #include "asm.h"
 #include <assert.h>
 #include <stdio.h>
-#include <setjmp.h>
 
 int main() {
   asm_jmp_buf buf;
-  int r = setjmp(buf);
+  int r = asm_setjmp(buf);
   if (r == 0) {
     assert(asm_add(1234, 5678) == 6912);
     assert(asm_popcnt(0x0123456789abcdefULL) == 32);
