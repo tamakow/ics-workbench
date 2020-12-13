4ef4ae4bc5050dbc469d3bba87607583c26f15eb
tracer-nju <tracer@nju.edu.cn>
2020-12-13 20:43:07 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 7f95a082ad164fc09f9b0518eefe4a21   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  20:43:07 up  1:26,  1 user,  load average: 0.74, 0.83, 0.80

--- asm/main.c
+++ asm/main.c
@@ -1,19 +1,18 @@
 #include "asm.h"
 #include <assert.h>
 #include <stdio.h>
+#include <setjmp.h>
 
 int main() {
   asm_jmp_buf buf;
-  int r = asm_setjmp(buf);
+  int r = setjmp(buf);
   if (r == 0) {
-    //assert(asm_add(1234, 5678) == 6912);
-    //assert(asm_popcnt(0x0123456789abcdefULL) == 32);
+    assert(asm_add(1234, 5678) == 6912);
+    assert(asm_popcnt(0x0123456789abcdefULL) == 32);
     // TODO: add more tests here.
-    printf("before longjmp\n");
-    asm_longjmp(buf, 1);
-    printf("after long jmp\n");
+    longjmp(buf, 1);
   } else {
-    //assert(r == 123);
+    assert(r == 123);
     printf("PASSED.\n");
   }
 }
