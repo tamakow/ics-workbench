dc0b88f4e151914db94b1be428a64620f2869d8d
tracer-nju <tracer@nju.edu.cn>
2020-12-13 23:41:50 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 7f95a082ad164fc09f9b0518eefe4a21   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  23:41:50 up  4:24,  1 user,  load average: 1.35, 0.84, 0.83

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -2,18 +2,41 @@
 #include <string.h>
 
 int64_t asm_add(int64_t a, int64_t b) {//返回有符号64位加法
-  asm("add %1,%0;"
-      : "+r"(a)
-      : "r"(b)
+  asm("add %1, %0;"
+      : "+r"(a)//%0
+      : "r"(b)//%1
   );
   return a;
 }
 
 int asm_popcnt(uint64_t x) {//无符号64位整数x二进制表示中1的数量
   int s = 0;
+  /*
   for (int i = 0; i < 64; i++) {
     if ((x >> i) & 1) s++;
   }
+  */
+ //先使用 gcc -S 将c代码转换至汇编代码再写内联汇编
+  asm("mov %1, -24(%%rbp);"//mov %rdi -24(%rbp) rdi寄存器中存储x 
+      "mov $0, -8(%%rbp);"
+      "mov $0, -4(%%rbp);" // i
+      "jmp L2;"
+      "L4: mov -4(%%rbp), %%eax;"//eax 存储 i
+      "mov -24(%%rbp), %rdx;" //rdx 存储 x
+      "mov %%eax, %%ecx;"//ecx存储i
+      "shr %%cl, %%rdx;" //逻辑右移
+      "mov %%rdx, %%rax;" 
+      "and $1, %%eax;"
+      "test %%rax, %%rax;"
+      "je L3;"
+      "addl $1, %0;"
+      "L3: addl $1, -4(%%rbp)"
+      "L2: cmpl $63, -4(%%rbp);"
+      "jle L4;"
+      : "+r"(s)
+      : "r"(x)
+      : "%rax", "%eax", "%ecx","%rdx","%cl"
+  );
   return s;
 }
 
@@ -28,3 +51,4 @@ int asm_setjmp(asm_jmp_buf env) {
 void asm_longjmp(asm_jmp_buf env, int val) {
   longjmp(env, val);
 }
+;
