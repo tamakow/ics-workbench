7ce10189fd38ddae2db0b14fd1963c3ed318b1d0
tracer-nju <tracer@nju.edu.cn>
2020-12-14 16:54:59 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: d7db8b69e5974f738ef58e6fab75f83f   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  16:54:59 up  8:05,  1 user,  load average: 2.47, 1.97, 1.83

--- asm/main.c
+++ asm/main.c
@@ -6,12 +6,16 @@ int main() {
   asm_jmp_buf buf;
   int r = asm_setjmp(buf);
   if (r == 0) {
+    //tests for asm_add
     assert(asm_add(1234, 5678) == 6912);
+    //tests for asm_popcnt
     assert(asm_popcnt(0x0123456789abcdefULL) == 32);
-    //assert(asm_popcnt(0xffffffffffffffffULL) == 64);
-    //assert(asm_popcnt(0xff) == 8);
+    assert(asm_popcnt(0xffffffffffffffffULL) == 64);
+    assert(asm_popcnt(0xff) == 8);
+    //test for asm_memcpy
+
     // TODO: add more tests here.
-    asm_longjmp(buf, 1);
+    asm_longjmp(buf, 123);
   } else {
     assert(r == 123);
     printf("PASSED.\n");
