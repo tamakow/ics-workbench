98ee2306a584a6a414f85be41619be9cb70f377b
tracer-nju <tracer@nju.edu.cn>
2020-12-14 15:00:46 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: d7db8b69e5974f738ef58e6fab75f83f   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  15:00:47 up  6:11,  1 user,  load average: 0.72, 0.94, 0.96

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -46,14 +46,14 @@ int asm_popcnt(uint64_t x) {//无符号64位整数x二进制表示中1的数量
 void *asm_memcpy(void *dest, const void *src, size_t n) {
   //return memcpy(dest, src, n);
    int d0, d1, d2;
-   asm volatile(//"rep ; movsl;"
+   asm volatile("rep ; movsl;"
                  "movl %4,%%ecx;"
-                 "andl $3,%%ecx;"
-                 "jz 1f;"
+                 //"andl $3,%%ecx;"
+                 //"jz 1f;"
                  "rep ; movsb;"
-                 "1:"
+                 //"1:"
                  : "=&c" (d0), "=&D" (d1), "=&S" (d2)
-                 : "0" (n / 4), "g" (n), "1" ((long)dest), "2" ((long)src)
+                 : "0" (n >> 4), "g" (n & 3), "1" (dest), "2" (src)
                  : "memory"
                 );
                 
