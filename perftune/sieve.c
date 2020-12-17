3f541aaee237419e03b8577e09394322da050e2f
tracer-nju <tracer@nju.edu.cn>
2020-12-17 10:02:05 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 3b1381d71a554a5a9d1ba9739d2ca5bf   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  10:02:06 up  1:00,  1 user,  load average: 0.74, 0.95, 0.96

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -30,10 +30,14 @@ int *sieve(int n) { //埃氏筛
 */
 int *sieve(int n){
   assert(n+1<N);
-  int tot=0;
-  primes[tot++]=2;
-	for(int i=3;i<=n;++i){
+  int tot=3;
+  primes[0]=2;
+  primes[1]=3;
+  primes[2]=5;
+	for(int i=6;i<=n;++i){
     if((i&1)==0) continue;
+    if(i%3==0) continue;
+    if(i%5==0) continue;
 		if(!is_prime[i]) primes[tot++]=i;
 		for(int j=0;j<tot && i*primes[j]<=n;++j){
 			is_prime[i*primes[j]]=true;
