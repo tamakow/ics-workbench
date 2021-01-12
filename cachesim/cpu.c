cc43ce01dba3973318745eadba5aa64c686ec98f
tracer-nju <tracer@nju.edu.cn>
2021-01-13 00:46:03 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: fda60f16c989454ca62b622e7a02085a   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  00:46:03 up  4:53,  1 user,  load average: 0.67, 0.80, 0.69

--- cachesim/cpu.c
+++ cachesim/cpu.c
@@ -11,6 +11,7 @@ const uint32_t len2datamask [] = { 0x0, 0xff, 0xffff, 0xffffff, 0xffffffff };
 
 uint32_t cpu_read(uintptr_t addr, int len) {
   cycle_increase(1);
+  printf("%d\n",cache_read(addr));
   return (cache_read(addr) >> addr_offset_bit(addr)) & len2datamask[len];
 }
 
