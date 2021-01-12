09e52e8fce6a3c9272fb15bd1abd464fc4a418fd
tracer-nju <tracer@nju.edu.cn>
2021-01-12 23:59:04 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: fda60f16c989454ca62b622e7a02085a   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  23:59:04 up  4:06,  1 user,  load average: 1.62, 1.22, 0.91

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -65,6 +65,51 @@ uint32_t cache_read(uintptr_t addr) {
 }
 
 void cache_write(uintptr_t addr, uint32_t data, uint32_t wmask) {
+  //得到块内地址
+  uint32_t block_addr = addr & mask_with_len(BLOCK_WIDTH);
+  //得到cache组号
+  uint32_t group_idx = (addr >> BLOCK_WIDTH) & mask_with_len(group_number_width);
+  //得到标记
+  uint32_t tag = (addr >> (BLOCK_WIDTH + group_number_width)) & mask_with_len(tag_width);
+  //得到对应组的行号范围
+  uint32_t line_start = group_idx * exp2(associate_width);
+  uint32_t line_end = line_start + exp2(associate_width);
+  for(int i = line_start; i< line_end; ++i){
+    cycle_increase(1);
+    if(cache[i].valid_bit&&(cache[i].tag == tag)){
+      cache[i].dirty_bit = true;
+      cache[i].data[block_addr] = (cache[i].data[block_addr] & ~wmask) | (data & wmask); 
+      return;
+    }
+  }
+  //miss
+  //得到在主存的块数
+  uint32_t block_idx = (addr >> BLOCK_WIDTH) & mask_with_len(25-BLOCK_WIDTH);
+  //寻找是否存在没有使用的cache行
+  for(int i = line_start; i< line_end; ++i){
+    cycle_increase(1);
+    if(!cache[i].valid_bit){ 
+      mem_read(block_idx, cache[i].data);
+      cache[i].valid_bit = true;
+      cache[i].tag = tag;
+      cache[i].dirty_bit = true;
+      cache[i].data[block_addr] = (cache[i].data[block_addr] & ~wmask) | (data & wmask); 
+      return;
+    }
+  }
+  //替换算法
+  uint32_t random_line = line_start + rand() % exp2(associate_width);
+  //不确定写对了没
+  uint32_t random_line_block_idx = (cache[random_line].tag << group_number_width) + group_idx;
+  if(cache[random_line].dirty_bit){ //cache内容被修改过，需要写回主存
+    mem_write(random_line_block_idx, cache[random_line].data);
+  }
+  mem_read(block_idx,cache[random_line].data);
+  cache[random_line].valid_bit = true;
+  cache[random_line].tag =tag;
+  cache[random_line].dirty_bit = true;
+  cache[random_line].data[block_addr] = (cache[random_line].data[block_addr] & ~wmask) | (data & wmask); 
+  return;
 }
 
 void init_cache(int total_size_width, int associativity_width) {
