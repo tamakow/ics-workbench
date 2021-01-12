23d001d4e9adbf43a5e1199d8c239416f0db1ca4
tracer-nju <tracer@nju.edu.cn>
2021-01-12 22:52:11 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: fda60f16c989454ca62b622e7a02085a   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  22:52:11 up  2:59,  1 user,  load average: 1.28, 0.89, 0.70

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -5,15 +5,19 @@ void mem_read(uintptr_t block_num, uint8_t *buf);
 void mem_write(uintptr_t block_num, const uint8_t *buf);
 
 static uint64_t cycle_cnt = 0;
-
+static uint32_t total_width; // cache的数据大小宽度
+static uint32_t associate_width; // cache关联度的宽度，即一个cache组里有2^associate_width个cache行
+static uint32_t total_line_number; //cache总共的行数
 static cache_line *cache;
+
 
 void cycle_increase(int n) { cycle_cnt += n; }
 
 // TODO: implement the following functions
-// 使用的是直接映射
+// 使用组相联映射
 uint32_t cache_read(uintptr_t addr) {
-  
+  //要得到标记，cache组号，块内地址，要先知道cache组号的宽度，标记的宽度这些东西
+
   return 0;
 }
 
@@ -21,6 +25,12 @@ void cache_write(uintptr_t addr, uint32_t data, uint32_t wmask) {
 }
 
 void init_cache(int total_size_width, int associativity_width) {
+  total_width = total_size_width;
+  associate_width = associativity_width;
+  //创建cache
+  total_line_number = exp2(total_size_width - BLOCK_WIDTH);
+  cache = (cache_line*)malloc(sizeof(cache_line)*total_line_number);
+  
 }
 
 void display_statistic(void) {
