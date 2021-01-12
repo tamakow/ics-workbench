4be1c6abe672b13cc0f53370a368651dc9c88b6d
tracer-nju <tracer@nju.edu.cn>
2021-01-13 00:47:04 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: fda60f16c989454ca62b622e7a02085a   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  00:47:04 up  4:54,  1 user,  load average: 0.66, 0.76, 0.68

--- cachesim/cpu.c
+++ cachesim/cpu.c
@@ -11,7 +11,7 @@ const uint32_t len2datamask [] = { 0x0, 0xff, 0xffff, 0xffffff, 0xffffffff };
 
 uint32_t cpu_read(uintptr_t addr, int len) {
   cycle_increase(1);
-  printf("%d\n",cache_read(addr));
+  printf("%u\n",cache_read(addr));
   return (cache_read(addr) >> addr_offset_bit(addr)) & len2datamask[len];
 }
 
@@ -21,7 +21,7 @@ void cpu_write(uintptr_t addr, int len, uint32_t data) {
 }
 
 uint32_t cpu_uncache_read(uintptr_t addr, int len) {
-  printf("%d\n",mem_uncache_read(addr));
+  printf("%u\n",mem_uncache_read(addr));
   return (mem_uncache_read(addr) >> addr_offset_bit(addr)) & len2datamask[len];
 }
 
