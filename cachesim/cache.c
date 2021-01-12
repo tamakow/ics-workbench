7690c50f17b30785dbb48c71d23e2f578ce267d7
tracer-nju <tracer@nju.edu.cn>
2021-01-12 23:04:02 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: fda60f16c989454ca62b622e7a02085a   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  23:04:03 up  3:11,  1 user,  load average: 0.62, 0.72, 0.71

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -5,9 +5,12 @@ void mem_read(uintptr_t block_num, uint8_t *buf);
 void mem_write(uintptr_t block_num, const uint8_t *buf);
 
 static uint64_t cycle_cnt = 0;
+
 static uint32_t total_width; // cache的数据大小宽度
 static uint32_t associate_width; // cache关联度的宽度，即一个cache组里有2^associate_width个cache行
 static uint32_t total_line_number; //cache总共的行数
+static uint32_t group_width; // cache组数量的宽度，及有2^group_width个cache组
+static uint32_t tag_width; // 标签的宽度
 static cache_line *cache;
 
 
@@ -30,7 +33,12 @@ void init_cache(int total_size_width, int associativity_width) {
   //创建cache
   total_line_number = exp2(total_size_width - BLOCK_WIDTH);
   cache = (cache_line*)malloc(sizeof(cache_line)*total_line_number);
-  
+  group_width = total_size_width - BLOCK_WIDTH - associativity_width;
+  tag_width = 25 - BLOCK_WIDTH - group_width;
+  for(int i = 0; i < total_line_number; ++i){
+    cache[i].valid_bit = false;
+    cache[i].dirty_bit = false;
+  }
 }
 
 void display_statistic(void) {
