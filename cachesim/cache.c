9b3e0d5dc7d4beb889052999effc1ae5586d578b
tracer-nju <tracer@nju.edu.cn>
2021-01-13 00:51:48 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: fda60f16c989454ca62b622e7a02085a   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  00:51:48 up  4:59,  1 user,  load average: 1.13, 0.82, 0.71

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -58,7 +58,7 @@ uint32_t cache_read(uintptr_t addr) {
   //不确定写对了没
   uint32_t random_line_block_idx = (cache[random_line].tag << group_number_width) + group_idx;
   if(cache[random_line].dirty_bit){ //cache内容被修改过，需要写回主存
-    mem_write(random_line_block_idx, cache[random_line].data);
+    mem_write(block_idx, cache[random_line].data);
   }
   mem_read(block_idx,cache[random_line].data);
   cache[random_line].valid_bit = true;
